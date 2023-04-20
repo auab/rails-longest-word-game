85080591328bc247ad278e52f27e52824bdba7f9
Pedro Abreu <pedro.aurelio.abreu@gmail.com>
2023-04-20 10:23:18 -0300

    create acore page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
 
   def new
@@ -6,10 +9,28 @@ class GamesController < ApplicationController
     10.times { @letters << alphabet_array.sample }
   end
 
-  def score
-    # @word = params[:word]
-    # word_splitted = @word.split('')
-    # if
+  def included?(guess, grid)
+    u = grid.count('a')
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
   end
 
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+
+  def score
+    @word = params[:word]
+    grid = params[:grid]
+    if included?(@word, grid)
+      if english_word?(@word)
+        @result = 'success'
+      else
+        @result = 'not english'
+      end
+    else
+      @result = 'not possible'
+    end
+  end
 end
